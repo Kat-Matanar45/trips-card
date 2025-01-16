f362c3e2bbb30e3ba4e3134a83ee916a864750ef
Kat <avchimchik@gmail.com>
2025-01-17 00:27:09 +0500

    Верстка перенесена на Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper mt-5">
+    <HeroView />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroView from './views/HeroView.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    HeroView
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
