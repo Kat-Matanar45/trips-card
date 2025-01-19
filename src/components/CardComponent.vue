a23bac76cb928fd0f2fde1e680e06c3076bbe746
Kat <avchimchik@gmail.com>
2025-01-19 17:30:54 +0500

    Обрезка и читать далее есть, но роутинг не работает

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,8 +2,12 @@
         <div class="item-wrapper d-flex flex-column">
             <img :src="require(`@/assets/img/${card.image}`)" class="mb-3" :alt="card.image">
             <h3>{{ card.title }}</h3>
-            <div class="description">
+            <div class="description" v-if='card.text.length < 550'>
                 {{ card.text }}
+            </div>
+            <div class="description" v-else>
+                {{ card.text.slice(0, 550) + '...' }}
+                <br><span>Читать далее...</span>
             </div>
         </div>
 </template>
